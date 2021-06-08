b9e05eed8a3ca7f025e0ca9fe4e899251827fa69
Carolin Meier <carolin.meier@cbc.de>
2021-06-08 15:52:42 +0200

    exercie components

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
-  styleUrls: ['./courses.component.scss']
+  styleUrls: ['./courses.component.scss'],
 })
 export class CoursesComponent implements OnInit {
   // CHALLENGE
@@ -11,26 +11,51 @@ export class CoursesComponent implements OnInit {
   // STEP 02: Add event handler to select course
   // STEP 03: Display raw json of selected course
 
+  public selectedCourse: string = null;
+
   courses = [
     {
       id: 1,
       title: 'Angular 9 Fundamentals',
       description: 'Learn the fundamentals of Angular 9',
       percentComplete: 26,
-      favorite: true
+      favorite: true,
     },
     {
       id: 2,
       title: 'JavaScript The Really REALLY HARD PARTS',
       description: 'Worship Will Sentance',
       percentComplete: 50,
-      favorite: true
-    }
+      favorite: true,
+    },
+    {
+      id: 3,
+      title: 'Java script',
+      description: 'blabla',
+      percentComplete: 75,
+      favorite: true,
+    },
+    {
+      id: 4,
+      title: 'Java script',
+      description: 'blabla',
+      percentComplete: 75,
+      favorite: true,
+    },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  selectCourse(course): void {
+    console.log(course);
+    this.selectedCourse = course;
   }
 
+  deleteCourse(courseId): void {
+    console.log(courseId);
+    this.courses.splice(this.courses.indexOf(courseId), 1);
+    console.log(this.courses.splice(this.courses.indexOf(courseId), 1));
+  }
 }
