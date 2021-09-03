a5f0f7242760c3a93f03d6bbc2df87774d6adc35
Carolin Meier <carolin.meier@cbc.de>
2021-09-03 14:23:49 +0200

    feat: implement state management

--- src/app/courses/courses-details/courses-details.component.ts
+++ src/app/courses/courses-details/courses-details.component.ts
@@ -6,9 +6,24 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./courses-details.component.scss'],
 })
 export class CoursesDetailsComponent implements OnInit {
-  @Input() selectedCourse;
+  changeCourse;
+
   @Output() saveCourse = new EventEmitter();
   @Output() cancel = new EventEmitter();
+
+  // state management
+  //  die aktulle Eingabe in eine Variable zwischenspeichern, damit sie nicht sofort in der courses-list sichtbar ist
+
+  // ich kriege den Input namens selectedCourse.
+  // dann müsste ich wahrscheinlich auch das ngModel ändern, wobei ne, nicht direkt im Inputfield.
+  // ich muss quasi beim output, also beim saveCourse was anderes mitschicken,als was ich beim selectedCourse reinbekomme
+
+  @Input() set selectedCourse(value) {
+    if (value) {
+      console.log(value);
+      this.changeCourse = Object.assign({}, value);
+    }
+  }
 
   constructor() {}
 
